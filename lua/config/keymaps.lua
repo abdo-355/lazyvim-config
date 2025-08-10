d38ec860790208d5af3e00639e78e5c543a19ad7
abdo-355 <abdoluxor366@gmail.com>
2025-08-10 13:58:22 +0300

    add ui code actions like the one from vs code

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
+
+-- for ui code actions
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
